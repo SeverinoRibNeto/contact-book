ad458678d99d25cad3a559e92f69a31ba067d015
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-02-09 18:17:03 -0300

    Add class variables; Add methods "Save" and "Find"

--- src/models/person.py
+++ src/models/person.py
@@ -1,13 +1,20 @@
 
 class Person:
+    name = ''
+    lastName = ''
+    birthday = ''
+    phoneNumber = ''
+    email = ''
+    adress = ''
 
-    def __init__(self, name, lastName, birthday, phoneNumber, email, adress):
-        self.name = name
-        self.lastName = lastName
-        self.birthday = birthday
-        self.phoneNumber = phoneNumber
-        self.email = email
-        self.adress = adress
+    def __init__(self):
+        return
 
     def completeName(self):
         return f'{self.name} {self.lastName}'
+
+    def savePerson(self):
+        return
+
+    def findPerson(self):
+        return
