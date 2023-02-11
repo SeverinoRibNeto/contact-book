2a25a5e711301987dca99a9c593be9d46c26c90d
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-02-10 21:24:32 -0300

    __init__ adjusted

--- src/models/person.py
+++ src/models/person.py
@@ -1,14 +1,13 @@
-
 class Person:
-    name = ''
-    lastName = ''
-    birthday = ''
-    phoneNumber = ''
-    email = ''
-    adress = ''
 
-    def __init__(self):
-        return
+    def __init__(self, name: str, phoneNumber: str, lastName: str = None,
+                 birthday=None,  email: str = None, address: str = None):
+        self.name = name
+        self.lastName = lastName
+        self.birthday = birthday
+        self.phoneNumber = phoneNumber
+        self.email = email
+        self.address = address
 
     def completeName(self):
         return f'{self.name} {self.lastName}'
