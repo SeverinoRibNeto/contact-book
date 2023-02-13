48298e5350ac7f42c4e2f75b95553375558fcd9d
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-02-12 23:52:46 -0300

    Insert codes to connect database

--- src/models/person.py
+++ src/models/person.py
@@ -1,8 +1,7 @@
-from bd import Database
+import sqlite3
 
 
 class Person:
-    database = Database()
     tableName = "contacts"
     headerColumns = ['name',
                      'lastName',
@@ -12,14 +11,21 @@ class Person:
                      'address']
     id = 0
 
-    def __init__(self, name: str, phoneNumber: str, lastName: str = None,
-                 birthday=None,  email: str = None, address: str = None):
+    def __init__(self, name: str = None, phoneNumber: str = None,
+                 lastName: str = None, birthday=None,
+                 email: str = None, address: str = None):
         self.name = name
         self.lastName = lastName
         self.birthday = birthday
         self.phoneNumber = phoneNumber
         self.email = email
         self.address = address
+        self.database = "contact-book.db"
+        try:
+            self.conn = sqlite3.connect(self.database)
+            self.cursor = self.conn.cursor()
+        except Exception as exception:
+            print(exception)
 
     def completeName(self):
         return f'{self.name} {self.lastName}'
@@ -34,14 +40,67 @@ class Person:
         listValues.append(self.address)
         return listValues
 
-    def save(self):
-        self.database.addIntoTable(
-            self.tableName, self.headerColumns, self.dataToList())
-        self.id = self.database.lastIdInserted(self.tableName)
+    def tableExists(self, tableName: str) -> bool:
+        tableList = []
+        try:
+            tables = self.cursor.execute(
+                """SELECT name FROM sqlite_master WHERE type='table';"""
+            ).fetchall()
+            # Sort names in list
+            for table in tables:
+                for name in table:
+                    tableList.append(name)
+            # check table in list
+            if (tableName in tableList):
+                return True
+        except Exception as exception:
+            print(exception)
+        return False
+
+    def lastIdInserted(self, tableName: str) -> int:
+        if(not self.tableExists(tableName)):
+            raise ValueError("Table '" + tableName + "' not exists")
+
+        data = self.cursor.execute(f"""
+                SELECT id
+                FROM {tableName}
+                WHERE   ID = (SELECT MAX(ID)  FROM {tableName});
+                """).fetchone()
+        return int(data[0])
+
+    def save(self) -> bool:
+        columnsTable = '('
+        valuesList = '('
+        # Organize the values into parentheses
+        for column in self.headerColumns:
+            columnsTable += column + ','
+        columnsTable = columnsTable[:-1] + ')'
+
+        for value in self.dataToList():
+            valuesList += f"'{value}',"
+        valuesList = valuesList[:-1] + ")"
+        if(not self.tableExists(self.tableName)):
+            raise ValueError("Table '" + self.tableName + "' not exists")
+
+        code = f"""INSERT INTO {self.tableName}
+                {columnsTable}
+                VALUES {valuesList};"""
+        try:
+            self.cursor.execute(code)
+            self.conn.commit()
+        except Exception as exception:
+            print(exception)
         return True
 
     def find(self):
-        data = self.database.findData(self.tableName, self.id)
+        if(not self.tableExists(self.tableName)):
+            raise ValueError("Table '" + self.tableName + "' not exists")
+        data = self.cursor.execute(
+            f"""
+               SELECT * FROM {self.tableName} WHERE id={id};
+            """
+        )
+        data = data.fetchone()
         self.id = data[0]
         self.name = data[1]
         self.lastName = data[2]
@@ -52,18 +111,35 @@ class Person:
         return True
 
     def delete(self):
-        self.database.deleteData(self.tableName, self.id)
+        if(not self.tableExists(self.tableName)):
+            raise ValueError("Table '" + self.tableName + "' not exists")
+        self.cursor.execute(f"""
+            DELETE FROM {self.tableName}
+            WHERE id = {self.id};
+                """)
+        self.conn.commit()
         return True
 
     def alter(self):
-        self.database.alterData(self.tableName,
-                                self.id,
-                                self.headerColumns,
-                                self.dataToList())
+        values = self.dataToList()
+        if(not self.tableExists(self.tableName)):
+            raise ValueError("Table '" + self.tableName + "' not exists")
+        strCode = ''
+        for cont in range(len(self.headerColumns)):
+            strCode += f"{self.headerColumns[cont]} = '{values[cont]}',"
+        strCode = strCode[:-1]
+        self.cursor.execute(f"""
+                UPDATE {self.tableName}
+                SET {strCode}
+                WHERE id = {self.id};
+                """)
+        self.conn.commit()
         return True
 
 
+"""
 if __name__ == "__main__":
     pessoa = Person("Cida", "4654654")
     pessoa.id = 2
     pessoa.find()
+"""
